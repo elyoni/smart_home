adbf704ef51caab06b6da18e39f3310a35fa4959
Yoni Elentok <yehonatan.e@solaredge.com>
2019-09-25 23:02:56 +0300

    Change the name of a file
    renamed:    PC-python/client.py -> PC-python/device.py

--- PC-python/device.py
+++ PC-python/device.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 from threading import Lock
 from time import sleep
 import sys
+import json
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(name)s: %(message)s',)
@@ -10,35 +11,83 @@ logger = logging.getLogger('MDevice')
 logger.setLevel(logging.INFO)
 
 
+class StateTopic:
+    def __init__(self, device_id, device_type):
+        self._base_topic = 'device/{0}/{1}'.format(device_type, device_id)
+
+    def connect(self):
+        return self._base_topic + "/connect"
+
+    def disconnect(self):
+        return self._base_topic + "/disconnect"
+
+    def set(self, value):
+        return self._base_topic + "/set/" + str(value)
+
+    def get(self, value):
+        return self._base_topic + "/disconnect"
+
+
+class Ticket:
+    """I am not using underline because I will create a json data
+    from the self variables."""
+    def __init__(self, device_id, device_type, location):
+        self.device_type = device_type
+        self.device_id = device_id
+        self.connection_status = None
+        self.location = location
+        self.last_codition = {}
+
+    def set_connection(self, connection):
+        self.connection_status = connection
+
+    """
+            ```JSON
+                {
+                    "connection_status": "off/on",
+                    "type": analog_lamp, digital_lamp, ... Amp_meter.
+                    "device_id": # of the device, unique, number for his type
+                    "location": the location if the device
+                    "last condition": {
+                            "condition": last value that gas been send from the device
+                            "timestamps": save the time that the last message has been sent
+                            "timestamps timeout": after that time the data will not be valid anymore"
+                        }
+                }	
+            ```
+    """
+
 class MDevice(mqtt.Client):
 
-    def __init__(self, device_id, device_type, ip="127.0.0.1", port=1883, user_settings=None):
+    def __init__(self, device_id, device_type, location, ip="127.0.0.1", port=1883, user_settings=None):
         logger.info('initializing...')
         self._ip = ip
         self._port = port
         self._user_settings = user_settings
         self._device_type = device_type
         # self._keepalive = keepalive
-        self._state_topic = 'device/{0}/{1}'.format(device_type, device_id)
+        self._state_topic = StateTopic(device_id, device_type)  # 'device/{0}/{1}'.format(device_type, device_id)
+        self._ticket = Ticket(device_id, device_type, location)
         self._ping_topic = 'pings'
-        print("state_topic:", self._state_topic)
+        print("Basic state_topic:", self._state_topic)
         print("ping_topic:", self._ping_topic)
 
         self._connected = False
         # creating a lock for the above var for thread-safe reasons
         self._lock = Lock()
 
-        #super(MDevice, self).__init__()
         super().__init__()
+
+    def get_ticket(self):
+        return json.dumps(self._ticket.__dict__)
 
     def connect(self):
         logger.info('connecting...')
         if self._user_settings is not None:
             self.username_pw_set(self._user_settings['user'], self._user_settings['pass'])
-        #self.will_set(self._state_topic, 'ungraceful_disconnection', qos=2)
-        self.will_set(self._state_topic, 'disconnected', qos=2)
+        self.will_set(self._state_topic.connect(), 'disconnected', qos=2)
 
-        #super(MDevice, self).connect(self._ip, self.port)
+        # super(MDevice, self).connect(self._ip, self.port)
         super().connect(self._ip, self._port)
         self.loop_start()
 
@@ -58,7 +107,7 @@ class MDevice(mqtt.Client):
                 return
 
         # inform the state server that the device will disconnect
-        self.publish(self._state_topic, 'graceful_disconnection', qos=2)
+        self.publish(self._state_topic.disconnect(), 'graceful_disconnection', qos=2)
         logger.info('publishing graceful_disconnection')
         # sleep for 3 secs so we receive TCP acknowledgement for the above message
         sleep(3)
@@ -72,7 +121,7 @@ class MDevice(mqtt.Client):
             logger.info('successful connection')
 
             # inform the state server that the device is connected
-            self.publish(self._state_topic, 'connected', qos=2)
+            self.publish(self._state_topic.connect(), self.get_ticket(), qos=2)
             # subscribe to the ping topic so when the server pings the device can respond with a pong
             self.subscribe(self._ping_topic, qos=2)
             with self._lock:
@@ -87,14 +136,14 @@ class MDevice(mqtt.Client):
         # when message is received from the ping topic respond with pong ('connected' state)
         if msg.topic == self._ping_topic:
             logger.info('received ping. responding with state')
-            self.publish(self._state_topic, 'connected', qos=2)
+            self.publish(self._state_topic.connect(), self.get_ticket(), qos=2)
 
 
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
         mdevice = MDevice(device_id=sys.argv[1], device_type="lamp")
     else:
-        mdevice = MDevice(device_id=0, device_type="lamp")
+        mdevice = MDevice(device_id=0, device_type="lamp", location="Living room")
     mdevice.connect()
 
     while True:
